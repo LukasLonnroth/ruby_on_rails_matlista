d0b4518f45f53f3708bd105686c3a4b0dfd8f3ac
LukasLonnroth <lukas.lonnroth@hotmail.se>
2018-12-29 19:15:25 +0200

    Add error handling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,36 +4,47 @@ require 'json'
 
 class WelcomeController < ApplicationController
   def index
-    response = RestClient.get('https://www.amica.fi/modules/json/json/Index?costNumber=3003&language=sv-FI')
-    json = JSON.parse(response)
-    menusForDays = json['MenusForDays']
-    today = menusForDays[0]
-    setmenus = today['SetMenus']
-    todayString = ""
+    begin
+      response = RestClient.get('https://www.amica.fi/modules/json/json/Index?costNumber=3003&language=sv-FI')
+      json = JSON.parse(response)
+      menusForDays = json['MenusForDays']
+      today = menusForDays[0]
+      setmenus = today['SetMenus']
+      todayString = ""
 
-    for value in setmenus
-      components = value['Components']
-      for value in components
-        todayString = todayString + value + '<br>'
+      for value in setmenus
+        components = value['Components']
+        for value in components
+          todayString = todayString + value + '<br>'
+        end
       end
+      @arcadaResponse = todayString
+      @arcadaOpen = menusForDays[0]['LunchTime']
+    rescue NoMethodError
+      @arcadaResponse = "Error in fetching data"
+      @arcadaOpen = "Closed"
     end
-    @arcadaResponse = todayString
-    @arcadaOpen = menusForDays[0]['LunchTime']
 
-    response = RestClient.get('https://www.fazerfoodco.fi/modules/json/json/Index?costNumber=3104&language=sv-FI')
-    json = JSON.parse(response)
-    menusForDays = json['MenusForDays']
-    today = menusForDays[0]
-    setmenus = today['SetMenus']
-    todayString = ""
+    begin
+      response = RestClient.get('https://www.fazerfoodco.fi/modules/json/json/Index?costNumber=3104&language=sv-FI')
+      json = JSON.parse(response)
+      menusForDays = json['MenusForDays']
+      today = menusForDays[0]
+      setmenus = today['SetMenus']
+      todayString = ""
 
-    for value in setmenus
-      components = value['Components']
-      for value in components
-        todayString = todayString + value + '<br>'
+      for value in setmenus
+        components = value['Components']
+        for value in components
+          todayString = todayString + value + '<br>'
+        end
       end
+      @diakResponse = todayString
+      @diakOpen = menusForDays[0]['LunchTime']
+    rescue NoMethodError
+      @diakResponse = "Error in fetching data"
+      @diakOpen = "Closed"
     end
-    @diakResponse = todayString
-    @diakOpen = menusForDays[0]['LunchTime']
+
   end
 end
